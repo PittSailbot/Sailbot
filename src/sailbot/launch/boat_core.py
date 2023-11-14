28fc6e8f0e75b9d0f508ff45042c9c82e90de9de
aaron4522 <rex4522@gmail.com>
2023-11-13 19:37:05 -0500

    Update launch files

--- src/sailbot/launch/boat_core.py
+++ src/sailbot/launch/boat_core.py
@@ -8,24 +8,13 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    os.environ[
-        "ROS_LOG_DIR"
-    ] = f"/workspace/ros_logs/{str(datetime.now()).replace(' ', '_')}"
-    os.environ[
-        "ROS_LOG_DIR_BASE"
-    ] = f"/workspace/ros_logs/{str(datetime.now()).replace(' ', '_')}"
+    os.environ["ROS_LOG_DIR"] = f"/workspace/ros_logs/{str(datetime.now()).replace(' ', '_')}"
+    os.environ["ROS_LOG_DIR_BASE"] = f"/workspace/ros_logs/{str(datetime.now()).replace(' ', '_')}"
 
     return LaunchDescription(
         [
-            DeclareLaunchArgument(
-                "log_level", default_value=TextSubstitution(text=str("INFO"))
-            ),
-            Node(
-                package="sailbot",
-                namespace="boat",
-                executable="drivers",
-                name="drivers",
-            ),
+            DeclareLaunchArgument("log_level", default_value=TextSubstitution(text=str("INFO"))),
+            Node(package="sailbot", namespace="boat", executable="drivers", name="drivers"),
             Node(package="sailbot", namespace="boat", executable="main", name="main"),
         ]
     )
