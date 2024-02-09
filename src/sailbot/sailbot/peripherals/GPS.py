81f2aba0eb2ae2660b9ee6aa03efe1116b8bc38b
aaron4522 <rex4522@gmail.com>
2024-02-09 01:14:01 -0500

    Refactor GPS

--- src/sailbot/sailbot/peripherals/GPS.py
+++ src/sailbot/sailbot/peripherals/GPS.py
@@ -1,8 +1,7 @@
 """
-interfaces with USB GPS sensor
+Interfaces with USB GPS sensor
 """
 # https://learn.adafruit.com/adafruit-ultimate-gps/circuitpython-parsing
-from time import sleep
 import os
 
 import gpsd
@@ -13,16 +12,13 @@ from std_msgs.msg import String
 
 class GPS(Node):
     """
-    Attributes:
-        latitude (float): the current latitude of the boat
-        longitude (float): the current longitude of the boat
+    Publishes to:
+        - /gps: (String) '40.321,20.321'
     """
 
     def __init__(self):
-        self.latitude = None
-        self.longitude = None
-        self._node = Node("GPS")
-        self.logging = self._node.get_logger()
+        super().__init__("gps")
+        self.logging = self.get_logger()
 
         # Can also use adafruit_gps module (but didn't work at competition?)
         # Prev GPS version w/ adafruit: https://github.com/SailBotPitt/SailBot/blob/a56a18b06cbca78aace1990a4a3ce4dfd8c7a847/GPS.py
@@ -31,38 +27,28 @@ class GPS(Node):
         if gpsd.get_current().mode < 2:
             self.logging.warning(f"No GPS fix")
 
-        super().__init__("GPS")
-        self.pub = self.create_publisher(String, "GPS", 10)
+        self.pub = self.create_publisher(String, "gps", 10)
         self.timer = self.create_timer(0.5, self.timer_callback)
 
     def timer_callback(self):
-        msg = String()
         packet = gpsd.get_current()
 
         if packet.mode >= 2:
-            self.latitude = packet.position()[0]
-            self.longitude = packet.position()[1]
+            latitude = packet.position()[0]
+            longitude = packet.position()[1]
 
-            msg.data = f"{self.latitude},{self.longitude}"
-            self.pub.publish(msg)
-            self.logging.info('Publishing: "%s"' % msg.data)
+            msg = f"{latitude},{longitude}"
+            self.logging.info(f"Publishing: '{msg}'")
+            self.pub.publish(String(msg))
+
         else:
             self.logging.warning(f"No GPS fix")
-
-    def __getattribute__(self, name):
-        """
-        if an attempt is made to access an attribute that does not exist, it will then attempt to get the attribute from gps object
-        this means that rather than using gps_object.gps.longitude, it is possible to use gps_object.longitude
-        """
-        try:
-            return super().__getattribute__(name)
-        except:
-            return self.gps.__getattribute__(name)
 
 
 def main(args=None):
     os.environ["ROS_LOG_DIR"] = os.environ["ROS_LOG_DIR_BASE"] + "/gps"
     rclpy.init(args=args)
+
     gps = GPS()
     rclpy.spin(gps)
 
