e4c642280ca5324673d820f568916889ae9e5dde
aaron4522 <rex4522@gmail.com>
2024-06-04 18:02:04 -0400

    Update Events - Changed DummyEvent.py to work with arbitrary list of Waypoints - Replace eventDefaults.ini with params_eventDefaults.yaml - Move next_gps pub responsibility to individual events to support non-go-to-waypoint event behavior

--- src/sailbot/sailbot/events/DummyEvent.py
+++ src/sailbot/sailbot/events/DummyEvent.py
@@ -1,8 +1,15 @@
-import importlib
-import math
+"""
+Template code for creating new events. Currently, code sails through a list of defined waypoints.
+
+The process for creating new events:
+    - Add parameters in config/params_eventDefaults.yaml
+    - Initialize parameters in __init__
+    - Subscribe to necessary sensors in __init__
+        - Set up callback functions to update local variables
+    - Add event to setup.py:entry_points
+    - Add event logic to event_loop
+"""
 import os
-import time
-import threading
 import json
 
 import rclpy
@@ -19,83 +26,86 @@ DOCKER = os.environ.get("IS_DOCKER", False)
 DOCKER = True if DOCKER == "True" else False
 folder = "sailbot.peripherals." if not DOCKER else "sailbot.virtualPeripherals."
 
+
 class DummyEvent(Event):
-    """
-    Attributes:
-        - target (Waypoint): the center of the search bounds
-    """
+    def __init__(self):
+        super().__init__()
 
-    required_args = []
+        # Initialize parameters from params_eventDefaults.yaml
+        self.configureParameters(expected={"Waypoints": [[]]})
 
-    def __init__(self, event_info):
-        """
-        Args:
-            - _event_info (list): list containing target
-                - expects [Waypoint(center_lat, center_long)]
-        """
-        super().__init__(event_info)
+        self.waypoint_queue = []
+        wps = list(self.get_parameter('Waypoints').value)
+        for wp in wps:
+            self.waypoint_queue.append(Waypoint(wp[0], wp[1]))
 
-        # Declare parameters and their default values
-        self.configureParameters()
-        
-        # Set data based on parameters
-        target = self.get_parameter('Waypoint1').value
-        self.target = Waypoint(target[0], target[1])
+        # Initialize ROS subscribers & publishers (sensors, messages, etc.)
+        self.next_gps_pub = self.create_subscription(String, "/boat/next_gps", 10)
+        self.target = Waypoint(self.waypoint_queue.pop())
 
-        self.queuedWaypoints = []
-        for param in ['Waypoint2', 'Waypoint3', 'Waypoint4']:
-            target = self.get_parameter(param).value
-            self.queuedWaypoints.append(Waypoint(target[0], target[1]))
+        self.gps_sub = self.create_subscription(String, "GPS", self.gps_callback, 10)
+        self.position = Waypoint(0, 0)
 
-        self.pub_queuedWaypoints = self.create_publisher(String, "queued_waypoints", 10)
-        timer_period = 10.0  # seconds
-        self.timer = self.create_timer(timer_period, self.publish_queued_waypoints)
+        self.waypoint_queue_pub = self.create_publisher(String, "queued_waypoints", 10)
 
-        self.set_event_subscription = self.create_subscription(
-            String, "set_event_target", self.ROS_setEventCallback, 10
-        )
+        # Create timer for ROS to execute 'event_loop' every 5s
+        self.timer = self.create_timer(5, self.event_loop)
 
-    def configureParameters(self):
+    def gps_callback(self, msg):
+        self.position = Waypoint.from_msg(msg)
+
+    def event_loop(self):
+        """Event logic. Executed continuously by ROS's timer."""
+        try:
+            if distance_between(self.position, self.target) < float(c.config["CONSTANTS"]["reached_waypoint_distance"]):
+                self.logging.info("Reached Waypoint")
+                self.target = self.waypoint_queue.pop()
+
+                if self.target is None:
+                    raise EventFinished("Reached all waypoints")
+
+            # Publish next gps to navigate towards
+            msg = String()
+            msg.data = self.target.to_msg()
+            self.next_gps_pub.publish(msg)
+
+            # Publish waypoints for sailbot website map
+            msg = String()
+            msg.data = json.dumps({'Waypoints': [self.target.toJson()] + [wp.toJson() for wp in self.waypoint_queue]})
+            self.waypoint_queue_pub.publish(msg)
+            self.logging.debug(F"{self.__class__.__name__} publishing next_GPS: {self.target}")
+
+        except EventFinished as e:
+            self.logging.info(f"{self.__class__.__name__} finished")
+            pass
+
+        except Exception as e:
+            self.logging.error(F"{self.__class__.__name__} failed to get next GPS with error: {e}")
+            target = String()
+            target.data = ""
+
+    def configureParameters(self, expected):
         descriptor = ParameterDescriptor(
             type=ParameterType.PARAMETER_DOUBLE_ARRAY,
             description='Latitude and longitude for the target waypoint'
         )
         default_value = None
-        self.declare_parameter('Waypoint1', default_value, descriptor)
-        self.declare_parameter('Waypoint2', default_value, descriptor)
-        self.declare_parameter('Waypoint3', default_value, descriptor)
-        self.declare_parameter('Waypoint4', default_value, descriptor)
+        self.declare_parameter('Waypoints', default_value, descriptor)
 
         # Verify the parameters were set when the Node was created
-        paramList = ['Waypoint1', 'Waypoint2', 'Waypoint3', 'Waypoint4']
-        for param in paramList:
+        for param in list(expected.keys()):
             paramValue = self.get_parameter(param).value
 
-            if paramValue == None:
-                self.logging.error(F"{self.__class__.__name__} parameters not set!")
-                raise Exception(F"{self.__class__.__name__} parameters not set!")
+            if paramValue is None:
+                self.logging.error(F"{self.__class__.__name__} parameters not set for '{param}'!")
+                raise Exception(F"{self.__class__.__name__} parameters not set for '{param}'!")
 
-            if len(paramValue) != 2:
-                self.logging.error(F"{self.__class__.__name__} parameters invalid!")
-                raise Exception(F"{self.__class__.__name__} parameters invalid!")
-
-    def next_gps(self):
-        return self.target
-    
-    def publish_queued_waypoints(self):
-        msg = String()
-        msg.data = json.dumps({'Waypoints': [self.target.toJson()] + [wp.toJson() for wp in self.queuedWaypoints]})
-        self.pub_queuedWaypoints.publish(msg)
-    
-    def ROS_setEventCallback(self, msg):
-        self.target = Waypoint.from_msg(msg)
-        
 
 def main(args=None):
     os.environ["ROS_LOG_DIR"] = os.environ["ROS_LOG_DIR_BASE"] + "/main"
     rclpy.init(args=args)
 
-    event = DummyEvent({})
+    event = DummyEvent()
 
     try:
         rclpy.spin(event)
--- src/sailbot/sailbot/utils/eventUtils.py
+++ src/sailbot/sailbot/utils/eventUtils.py
@@ -13,147 +13,31 @@ EVENT_DICT_INITIALIZED = False
 
 
 class Event(Node):
-    """
-    Basic blueprint for creating new events
+    """Basic blueprint for creating new events"""
 
-    Attributes:
-        - event_info (array) - provided starter information about the event
-            - event_info = []
-
-    Functions:
-        - next_gps() - event logic which determines where to sail to next
-    """
-
-    base_required_args = []
-    required_args = {}
-
-    def __init__(self, event_info):
-        self._event_info = event_info
+    def __init__(self):
         super().__init__(self.__class__.__name__)
         self.logging = self.get_logger()
 
         self.logging.info(f"Initializing {self.__class__.__name__}")
 
-        for key in self.required_args:
-            if key not in event_info:
-                raise KeyError(f"{self.__class__.__name__} expects dict with key: {key}, keys found: {event_info.keys()}")
+        self.target = None
 
-        for key in self.base_required_args:
-            if key not in event_info:
-                raise KeyError(f"Events expect dict with key: {key}, keys found: {event_info.keys()}")
-
-        for key in event_info.keys():
-            if key not in self.required_args and key not in self.base_required_args:
-                self.logging.warning(f"Found unused key in {self.__class__.__name__}: {key}")
-
-
-        self.node_shutdown_sub = self.create_subscription(
-            String, "event_shutdown", self.event_stop_callback, 10
-        )
-        self.pub = self.create_publisher(String, "next_gps", 10)
-        timer_period = 5.0  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.node_shutdown_sub = self.create_subscription(String, "event_shutdown", self.event_stop_callback, 10)
+        self.set_event_sub = self.create_subscription(String, "set_event_target", self.ROS_setEventCallback, 10)
 
     def event_stop_callback(self, msg):
         raise KeyboardInterrupt()
 
-    def timer_callback(self):
-        try:
-            waypoint = self.next_gps()
-            target = waypoint.to_msg()
-        except Exception as e:
-            self.logging.error(F"{self.__class__.__name__} failed to get next GPS with error: {e}")
-            target = String()
-            target.data = ""
-
-        self.pub.publish(target)
-        self.logging.debug(F"{self.__class__.__name__} publishing next_GPS: {waypoint}")
+    def ROS_setEventCallback(self, msg):
+        self.target = Waypoint.from_msg(msg)
 
     @abstractmethod
-    def next_gps(self):
-        """
-        Main event script logic. Executed continuously by boatMain.
-
-        Returns either:
-            - The next GPS point that the boat should sail to stored as a Waypoint object
-            - OR None to signal the boat to drop sails and clear waypoint queue
-            - OR EventFinished exception to signal that the event has been completed
-        """
-
+    def event_loop(self):
+        """Main event script logic. Executed continuously by ROS."""
         raise NotImplementedError
 
 
 class EventFinished(Exception):
     """Signals that the event is finished and that it is safe to return to manual control"""
-
     pass
-
-
-def getEventDict():
-    global EVENT_DICT_INITIALIZED, _EVENT_DICT
-    if not EVENT_DICT_INITIALIZED:
-        __generateEventDict()
-
-    return _EVENT_DICT
-
-
-def __generateEventDict():
-    """
-    waits to import the events to prevent circular dependencies
-    """
-    import sailbot.events.endurance as endurance
-    import sailbot.events.precisionNavigation as precisionNavigation
-    import sailbot.events.search as search
-    import sailbot.events.stationKeeping as stationKeeping
-
-    eventDict = {
-        c.config["MODES"]["MOD_RC"]: lambda *args: None,
-        c.config["MODES"][
-            "MOD_PRECISION_NAVIGATE"
-        ]: precisionNavigation.PrecisionNavigation,
-        c.config["MODES"]["MOD_ENDURANCE"]: endurance.Endurance,
-        c.config["MODES"]["MOD_STATION_KEEPING"]: stationKeeping.StationKeeping,
-        c.config["MODES"]["MOD_SEARCH"]: search.Search,
-    }
-    global EVENT_DICT_INITIALIZED, _EVENT_DICT
-    EVENT_DICT_INITIALIZED = True
-    _EVENT_DICT = eventDict
-
-
-class EventDefaults:
-    """
-    defaults are stored as a class variable so that they are only initialized once
-    """
-    defaults = configparser.ConfigParser()
-    defaults.read(f"{c.root_dir}/eventDefaults.ini")
-
-
-def getDefaultEventParams(eventEnum):
-    """
-    generated a event_info dict using the default values found in eventDefaults.ini
-    """
-    key = None
-    for testKey, testValue in c.config["MODES"].items():
-        if testValue == eventEnum:
-            key = testKey
-            break
-
-    if key is None:
-        raise KeyError(f"{eventEnum} was not found in config['MODES']")
-
-    defaults = EventDefaults.defaults[key.upper()]
-
-    event_info = {}
-    for key, value in defaults.items():
-        if str(value).lower().startswith("waypoint"):
-            args = value.split(" ")[1:]
-            event_info[key] = Waypoint(float(args[0]), float(args[1]))
-
-        elif str(value).lower().startswith("float"):
-            floatVal = value.split(" ")[1]
-            event_info[key] = float(floatVal)
-
-        else:
-            event_info[key] = value
-
-    return event_info
