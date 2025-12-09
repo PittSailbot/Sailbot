727c569eedd587d25960ab37156f5e6900304cde
aaron4522 <rex4522@gmail.com>
2025-12-08 19:43:14 -0500

    Switch to cytron pinout - Pico2 flash/bootloader may have corrupted BOOT shows in device manager but no way to flash

--- src/teensy/src/layouts/breadboard.h
+++ src/teensy/src/layouts/breadboard.h
@@ -6,11 +6,11 @@
 // COMPONENTS
 // ============================================================================
 
-#define HAL_MICROCONTROLLER MCU_PICO2
+#define HAL_MICROCONTROLLER MCU_CYTRON
 #define HAL_RECEIVER RECEIVER_SBUS
-// #define HAL_SERVO_TYPE              SERVO_PROTOCOL_GPIO
-// #define HAL_SAIL_SERVO_SPEC         ServoSpecs::JOY880545
-// #define HAL_RUDDER_SERVO_SPEC       ServoSpecs::JOY881504
+#define HAL_SERVO_TYPE SERVO_PROTOCOL_GPIO
+#define HAL_SAIL_SERVO_SPEC ServoSpecs::JOY880545
+#define HAL_RUDDER_SERVO_SPEC ServoSpecs::JOY881504
 // #define HAL_JIB_SERVO_SPEC       // No jib servo on mini boat
 #define HAL_IMU IMU_BNO055
 #define HAL_GPS GPS_ADAFRUIT_PA1010D
@@ -22,19 +22,20 @@
 // ============================================================================
 
 // Servo pins
-#define SAIL_SERVO_PIN 9
-#define RUDDER_SERVO_PIN 10
-#define JIB_SERVO_PIN 11
+#define SAIL_SERVO_PIN 0
+#define RUDDER_SERVO_PIN 2
+#define JIB_SERVO_PIN 255
 
 // Sensor pins
 #define WINDVANE_ENCODER_A_PIN 6
 #define WINDVANE_ENCODER_B_PIN 7
 
 // Communication pins
-#define I2C_SDA_PIN 0
-#define I2C_SCL_PIN 1
-#define TRANSCEIVER_SERIAL &Serial2  // GP9
-#define GPS_SERIAL &Serial1
+#define I2C_SDA_PIN 16
+#define I2C_SCL_PIN 17
+#define TRANSCEIVER_RX_PIN 29  // GP5 for SerialPIO SBUS on RP2040 (avoid conflicts)
+// #define TRANSCEIVER_SERIAL &Serial  // GP9 for Teensy
+// #define GPS_SERIAL &Serial // Init GPS over UART
 
 // Water sensor I2C addresses
 #define WATER_SENSOR1_ADDR 0x36
