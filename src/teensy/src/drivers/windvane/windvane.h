33d69260b035cc835bb9f030c670ccafe3e7db6b
Kirk <kjm206@pitt.edu>
2026-02-16 20:07:24 -0500

    windvane driver

--- src/teensy/src/drivers/windvane/windvane.h
+++ src/teensy/src/drivers/windvane/windvane.h
@@ -1,8 +1,28 @@
-#ifndef WINDVANE_H
-#define WINDVANE_H
+#pragma once
+
 #include "teensy.pb.h"
+#include <Arduino.h>
 
-extern void setupWindVane();
-extern bool readWindVane(WindVane*);
+class RotaryWindVane {
+ private:
+  uint8_t pinA;
+  uint8_t pinB;
+  
+  volatile int encoderValue;
+  volatile bool hasChanged;
+  float filteredEncoderValue;
+  
+  static const int ENCODER_ROTATION = 256;
+  static const float FILTER_ALPHA;
+  
+  // Static instance pointer for ISR
+  static RotaryWindVane* instance;
+  
+  int filterValue(int rawValue);
+  static void encoderISR();
 
-#endif
+ public:
+  RotaryWindVane(uint8_t encoderPinA, uint8_t encoderPinB);
+  bool begin();
+  bool read(WindVane* data);
+};
