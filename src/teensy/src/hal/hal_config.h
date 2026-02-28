6086974f776b1c1d85037ad6ea2138ac586423cd
aaron4522 <rex4522@gmail.com>
2026-02-28 06:15:37 -0500

    Add protoboard layout

--- src/teensy/src/hal/hal_config.h
+++ src/teensy/src/hal/hal_config.h
@@ -20,6 +20,7 @@
 // #define DAN_MARINA_TOM_PCB  // Main boat + Teensy 4.1 + Tom PCB
 // #define DAN_MARINA_LEO_PCB   // Mini boat + Pico + Leo PCB
 // #define DRAGONFORCE_65      // Mini boat autonomy testing platform
+// #define PROTOBOARD            // Prototype board for Mini Boat PCB
 #define BREADBOARD
 
 // ===== PLATFORM CONFIGURATIONS =====
@@ -36,6 +37,9 @@
 
 #elif defined(DRAGONFORCE_65)
 #include "layouts/dragonforce_65.h"
+
+#elif defined(PROTOBOARD)
+#include "layouts/protoboard.h"
 
 #elif defined(BREADBOARD)
 #include "layouts/breadboard.h"
